ac187ac9e3994c6aba025e82e35531c51e90f102
odonnellryanc <odonnellryanc@gmail.com>
2015-04-29 09:49:12 -0400

    Fix accidental revert.

--- routes/company.js
+++ routes/company.js
@@ -4,6 +4,9 @@ var router = express.Router();
 
 router.route('/')
   .post(function (req, res) {
+    // I'd like to look into this more to see if doing things
+    // in this manner fits in with the event loop model
+    // of Node.js. This needs testing.
     db_mods.createCompany(req, res);
   })
   .get(function (req, res) {
